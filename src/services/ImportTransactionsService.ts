38a69c77bc97fcd9b12b21b48ffbba29f52aa9a9
Felipe Dias <felipedias.dev@gmail.com>
2020-09-03 23:25:41 -0300

    Add ImportTransactionsService implementation

--- src/services/ImportTransactionsService.ts
+++ src/services/ImportTransactionsService.ts
@@ -1,8 +1,65 @@
+import csvParse from 'csv-parse';
+import fs from 'fs';
+
+import { In, getCustomRepository, getRepository } from 'typeorm';
 import Transaction from '../models/Transaction';
+import AppError from '../errors/AppError';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import Category from '../models/Category';
+
+interface TransactionCSV {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+  category: string;
+}
 
 class ImportTransactionsService {
-  async execute(): Promise<Transaction[]> {
-    // TODO
+  async execute(csvFilePath: string): Promise<Transaction[]> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+    const categoriesRepository = getRepository(Category);
+    const readCSVStream = fs.createReadStream(csvFilePath);
+    const parseStream = csvParse({
+      from_line: 2,
+      ltrim: true,
+      rtrim: true,
+    });
+    const parseCSV = readCSVStream.pipe(parseStream);
+    const transactions: TransactionCSV[] = [];
+    const categories: string[] = [];
+
+    parseCSV.on('data', async line => {
+      const [title, value, type, category] = line.map((cell: string) =>
+        cell.trim(),
+      );
+
+      if (!title || !value || !type) {
+        throw new AppError('There is at least one data missing!');
+      }
+
+      transactions.push({ title, value, type, category });
+      categories.push(category);
+    });
+
+    await new Promise(resolve => parseCSV.on('end', resolve));
+
+    const setCategories = new Set(categories);
+    const uniqueCategories = Array.from(setCategories);
+
+    const existentCategories = await categoriesRepository.find({
+      where: { title: In(categories) },
+    });
+    const existentCategoryTitles = existentCategories.map(
+      (category: Category) => category.title,
+    );
+    const addCategoryTitles = uniqueCategories.filter(
+      category => !existentCategoryTitles.includes(category),
+    );
+    const newCategories = categoriesRepository.create(
+      addCategoryTitles.map(title => ({ title })),
+    );
+
+    await categoriesRepository.save(newCategories);
   }
 }
 
